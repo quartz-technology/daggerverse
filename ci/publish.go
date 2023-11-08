92451335f1869de9778e8effe62c4df2c880f7e0
Vasek - Tom C <tom@quartz.technology>
2023-11-08 17:54:18 +0100

    feat(ci): publish all modules
    Signed-off-by: Vasek - Tom C <tom@quartz.technology>

--- ci/publish.go
+++ ci/publish.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"strings"
 )
 
 func (c *Ci) Publish(ctx context.Context) (string, error) {
-	//modules := source()
+	modules := repo()
 
-	//	directories := []string{"golang", "golangci-lint", "node", "redis", "postgres", "launcher"}
+	directories := []string{"golang", "golangci-lint", "node", "redis", "postgres", "launcher"}
 
-	//	eg, gctx := errgroup.WithContext(ctx)
+	eg, gctx := errgroup.WithContext(ctx)
 
-	return dag.Launcher().Publish(ctx, repo(), LauncherPublishOpts{Path: "launcher"})
+	var ref []string
+
+	for _, d := range directories {
+		eg.Go(func() error {
+			out, err := dag.Launcher().Publish(gctx, modules, LauncherPublishOpts{Path: d})
+
+			ref = append(ref, out)
+			return err
+		})
+	}
+
+	err := eg.Wait()
+	if err != nil {
+		return "failed to publish modules", err
+	}
+
+	return fmt.Sprintf("modules published:\n %s", strings.Join(ref, "\n")), nil
 }
