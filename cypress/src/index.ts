870be4886f32061f927d5bddfaf508c4f3643099
Vasek - Tom C <tom@quartz.technology>
2024-02-12 23:39:50 +0100

    feat: add cypress module base
    Signed-off-by: Vasek - Tom C <tom@quartz.technology>

---
+++ cypress/src/index.ts
@@ -0,0 +1,27 @@
+import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+
+@object()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class Cypress {
+  /**
+   * example usage: "dagger call container-echo --string-arg yo stdout"
+   */
+  @func()
+  containerEcho(stringArg: string): Container {
+    return dag.container().from("alpine:latest").withExec(["echo", stringArg])
+  }
+
+  /**
+   * example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
+   */
+  @func()
+  async grepDir(directoryArg: Directory, pattern: string): Promise<string> {
+    return dag
+      .container()
+      .from("alpine:latest")
+      .withMountedDirectory("/mnt", directoryArg)
+      .withWorkdir("/mnt")
+      .withExec(["grep", "-R", pattern, "."])
+      .stdout()
+  }
+}
