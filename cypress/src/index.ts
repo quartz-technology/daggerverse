3ba6edd19c31e8f2fd111a22937ef471daf28908
Vasek - Tom C <tom@quartz.technology>
2024-02-14 00:31:02 +0100

    feat: add cypress module
    Signed-off-by: Vasek - Tom C <tom@quartz.technology>

--- cypress/src/index.ts
+++ cypress/src/index.ts
@@ -1,4 +1,4 @@
-import { dag, Container, Directory, object, func, field, Service } from "@dagger.io/dagger"
+import { dag, Container, Directory, object, func, field, Service } from '@dagger.io/dagger';
 
 @object()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -14,7 +14,7 @@ class Cypress {
    * the website.
    */
   @field()
-  website: Service;
+  website?: Service;
 
   /**
    * Port the website is exposed on.
@@ -22,27 +22,41 @@ class Cypress {
   @field()
   port: number = 8080;
 
-  constructor(source: Directory, website: Service, port?: number) {
+  constructor(source: Directory, website?: Service, port?: number) {
     this.source = source;
     this.website = website;
-    this.port = port ?? this.port
+    this.port = port ?? this.port;
   }
 
   /**
    * Run e2e tests on cypress.
-   * 
+   *
    * Note: The end to end test should do test using `BASE_URL` environment
    * to point to the website service.
-   * 
+   * If no website is provided, create a new one (only work for node application).
+   *
+   * TODO(TomChv): Take an interface Builder with a method `Build` to handle that for any language.
+   * TODO(TomChv): Add param to run specific tests.
+   * TODO(TomChv): Add param to not only run e2e tests.
+   *
    * Example usage: `dagger call run`
    */
   @func()
   async run(): Promise<string> {
-    return this.base().stdout();
-  }
+    // Build the website if it's not provided by the user
+    if (!this.website) {
+      const distSrc = dag
+        .node()
+        .withNpm()
+        .withSource(this.source)
+        .install()
+        .commands()
+        .build()
+        .directory('dist');
 
-  @func()
-  base(): Container {
+      this.website = dag.nginx(distSrc).expose();
+    }
+
     return dag
       .container()
       .from('cypress/included:13.6.2')
@@ -50,6 +64,8 @@ class Cypress {
       .withDirectory('/e2e', this.source)
       .withWorkdir('/e2e')
       .withExec(['npm', 'install'], { skipEntrypoint: true })
-      .withExec(['--env', `BASE_URL=http://website:${this.port}`]);
+      // TODO(TomChv): Find a way to make it generic (right now it's assume cypress fetch the app URL from that env var)
+      .withExec(['--env', `BASE_URL=http://website:${this.port}`])
+      .stdout();
   }
 }
