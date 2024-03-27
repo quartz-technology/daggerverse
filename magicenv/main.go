a763992a36ec15096cf9285f49776bf3de71f92f
Vasek - Tom C <tom@quartz.technology>
2024-03-27 22:08:05 +0100

    feat: add magicenv module
    Signed-off-by: Vasek - Tom C <tom@quartz.technology>

---
+++ magicenv/main.go
@@ -0,0 +1,54 @@
+// Magicenv provides utility functions to inject environment from various sources.
+//
+// Most of modern application use environment to configure runtime variable, such as
+// database connection, API key, etc.
+// Usually, they are store in a file, such as .env, .env.local, .env.development, or .envrc.
+//
+// This package provides a way to inject those environment into a container so you do not have
+// to redifine all your variable when you "daggerize" your application.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-envparse"
+)
+
+type Magicenv struct{}
+
+// Load environment from a .env or .envrc type of tile and inject it into a container.
+//
+// All variables are loaded as secret since we cannot guarantee the security of the file.
+func (m *Magicenv) LoadEnv(
+	ctx context.Context,
+
+	// The container to inject the environment in.
+	ctr *Container,
+	// The path to the environment file to load.
+	//
+	//+optional
+	//+default=.env
+	path string,
+) (*Container, error) {
+	// Get the content of the .env file
+	envFile, err := ctr.File(path).Contents(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", path, err)
+	}
+
+	// Parse it to get a map
+	envMap, err := envparse.Parse(strings.NewReader(envFile))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse environment file: %w", err)
+	}
+
+	// Inject all the variables as secret
+	for key, value := range envMap {
+		ctr = ctr.WithSecretVariable(key, dag.SetSecret(key, value))
+	}
+
+	return ctr, nil
+}
